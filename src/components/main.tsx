0c987d5becce852f1f1eb9ac0a98ca9b197e4efd
KavyaDindu <siridindu@gmail.com>
2024-06-15 13:22:09 -0700

    fix compilation issues

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { IconBrandLinkedin, IconMail, IconPhone } from "@tabler/icons-react";
 
--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const Resume = () => {
   const resumeUrl = "src/assets/testResume.pdf";
