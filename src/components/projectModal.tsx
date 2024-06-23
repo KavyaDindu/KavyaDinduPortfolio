a0ebb43241a021191662c3f119fd676090f34ddc
KavyaDindu <siridindu@gmail.com>
2024-06-22 20:06:12 -0700

    added projects

--- src/components/projectModal.tsx
+++ src/components/projectModal.tsx
@@ -1,78 +1,87 @@
-import React from "react";
-import AnimatedMetric from "./animatedMetric";
+import React from 'react';
+import AnimatedMetric from './animatedMetric';
 
 export interface Project {
-  image: string;
-  title: string;
-  companyName: string;
-  metricValue: number;
-  metricDescription: string;
-  problem: string;
-  solution: string;
+    mainImage: string;
+    images: string[];
+    title: string;
+    companyName: string;
+    metricValue: number;
+    metricDescription: string;
+    problem: string;
+    solution: string;
 }
 
 interface ProjectModalProps {
-  project: Project | null;
-  onClose: () => void;
+    project: Project | null;
+    onClose: () => void;
 }
 
 const ProjectModal: React.FC<ProjectModalProps> = ({ project, onClose }) => {
-  if (!project) return null;
+    if (!project) return null;
 
-  const handleOutsideClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if (event.currentTarget === event.target) {
-      onClose();
-    }
-  };
+    const handleOutsideClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        if (event.currentTarget === event.target) {
+            onClose();
+        }
+    };
 
-  return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center px-4 py-2 z-50"
-      onClick={handleOutsideClick}
-    >
-      <div className="bg-white p-4 sm:p-5 rounded-lg shadow-2xl flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl max-h-[100vh] relative overflow-auto animate-fadeIn">
-        <button
-          onClick={onClose}
-          className="absolute top-3 right-3 text-gray-500 hover:text-gray-800"
+    return (
+        <div
+            className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center px-4 py-2 z-50"
+            onClick={handleOutsideClick}
         >
-          <span className="text-xl md:text-2xl">&times;</span>
-        </button>
-        <div className="flex flex-col items-center p-1 sm:p-1">
-          <div className="flex items-center justify-between w-full mb-2 sm:mb-4">
-            <div>
-              <h2 className="text-lg sm:text-xl md:text-3xl font-bold text-[#001b5e]">
-                {project.title}
-              </h2>
-              <h3 className="text-sm sm:text-md md:text-2xl font-semibold text-gray-700">
-                {project.companyName}
-              </h3>
+            <div className="bg-white p-4 sm:p-5 rounded-lg shadow-2xl flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl max-h-[100vh] relative overflow-auto animate-fadeIn">
+                <button
+                    onClick={onClose}
+                    className="absolute top-3 right-3 text-gray-500 hover:text-gray-800"
+                >
+                    <span className="text-xl md:text-2xl">&times;</span>
+                </button>
+                <div className="flex flex-col items-center p-1 sm:p-1">
+                    <div className="flex items-center justify-between w-full mb-2 sm:mb-4">
+                        <div>
+                            <h2 className="text-lg sm:text-xl md:text-3xl font-bold text-[#001b5e]">
+                                {project.title}
+                            </h2>
+                            <h3 className="text-sm sm:text-md md:text-2xl font-semibold text-gray-700">
+                                {project.companyName}
+                            </h3>
+                        </div>
+                        <AnimatedMetric
+                            value={project.metricValue}
+                            description={project.metricDescription}
+                        />
+                    </div>
+                    <div className="w-full mb-2 sm:mb-4 flex justify-center">
+                        <img
+                            src={project.mainImage}
+                            alt={project.title}
+                            className="w-full max-w-xs sm:max-w-md md:max-w-md lg:max-w-lg xl:max-w-xl h-auto rounded-lg"
+                        />
+                    </div>
+                    <div className="w-full mb-2 sm:mb-4">
+                        <h4 className="text-lg sm:text-xl md:text-2xl font-semibold text-[#001b5e]">
+                            Problem
+                        </h4>
+                        <p className="text-sm sm:text-base md:text-lg">
+                            {project.problem}
+                        </p>
+                    </div>
+                    <div className="w-full">
+                        <h4 className="text-lg sm:text-xl md:text-2xl font-semibold text-[#001b5e]">
+                            Solution
+                        </h4>
+                        <p className="text-sm sm:text-base md:text-lg">
+                            {project.solution}
+                        </p>
+                    </div>
+                </div>
             </div>
-            <AnimatedMetric
-              value={project.metricValue}
-              description={project.metricDescription}
-            />
-          </div>
-          <div className="w-full mb-2 sm:mb-4 flex justify-center">
-            <img
-              src={project.image}
-              alt={project.title}
-              className="w-full max-w-xs sm:max-w-md md:max-w-md lg:max-w-lg xl:max-w-xl h-auto rounded-lg"
-            />
-          </div>
-          <div className="w-full mb-2 sm:mb-4">
-            <h4 className="text-lg sm:text-xl md:text-2xl font-semibold text-[#001b5e]">Problem</h4>
-            <p className="text-sm sm:text-base md:text-lg">{project.problem}</p>
-          </div>
-          <div className="w-full">
-            <h4 className="text-lg sm:text-xl md:text-2xl font-semibold text-[#001b5e]">Solution</h4>
-            <p className="text-sm sm:text-base md:text-lg">{project.solution}</p>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProjectModal;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,95 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { Carousel } from '@mantine/carousel';
-import testImage from "/assets/ProjectExample.png";
 import { IconArrowRight, IconArrowLeft } from '@tabler/icons-react';
-import ProjectItem from "./projectItem";
-import ProjectModal, { Project } from "./projectModal";
-import { rem } from "@mantine/core";
-
-
+import ProjectItem from './projectItem';
+import ProjectModal, { Project } from './projectModal';
+import { rem } from '@mantine/core';
+import { projectData } from './projectData';
 
 const Projects: React.FC = () => {
-  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-  const [projects, setProjects] = useState<Project[]>([]);
+    const [selectedProject, setSelectedProject] = useState<Project | null>(
+        null
+    );
+    const [projects, setProjects] = useState<Project[]>([]);
 
-  useEffect(() => {
-    const fetchedProjects: Project[] = [
-      {
-        image: testImage,
-        title: "Project 1",
-        companyName: "Company 1",
-        metricValue: 30,
-        metricDescription: "Revenue Increase",
-        problem: "Lorem ipsum dolor sit amet...",
-        solution: "Lorem ipsum dolor sit amet...",
-      },
-      {
-        image: testImage,
-        title: "Project 2",
-        companyName: "Company 2",
-        metricValue: 40,
-        metricDescription: "User Engagement Increase",
-        problem: "Lorem ipsum dolor sit amet...",
-        solution: "Lorem ipsum dolor sit amet...",
-      },
-      // More projects...
-    ];
-    setProjects(fetchedProjects);
-  }, []); 
+    useEffect(() => {
+        setProjects(projectData);
+    }, []);
 
-  return (
-    <div id="projects" className="max-w-[1040px] m-auto px-4 py-0 md:pl-20">
-      <h1 className="text-4xl font-bold text-center text-blue-900 mb-8">
-        Projects
-      </h1>
-      {projects.length > 0 ? (
-        <Carousel
-          withIndicators
-          withControls
-          controlsOffset="xl"
-          controlSize={40}
-                nextControlIcon={<IconArrowRight style={{ width: rem(26), height: rem(26) }} />}
-      previousControlIcon={<IconArrowLeft style={{ width: rem(26), height: rem(26) }} />}
-          slideSize="80%"
-          slideGap="lg"
-            styles={{
-    control: {
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-      borderRadius: '9999px',
-      width: '50px', 
-      height: '50px', 
-      display: 'flex',
-      alignItems: 'center', 
-      justifyContent: 'center', 
-      color: 'white', 
-    },
-    indicator:{
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-    }
-  }}
-        >
-          {projects.map((project) => (
-            <Carousel.Slide key={project.title}>
-              <ProjectItem
-                image={project.image}
-                title={project.title}
-                description={project.companyName}
-                onMoreInfo={() => setSelectedProject(project)}
-              />
-            </Carousel.Slide>
-          ))}
-        </Carousel>
-      ) : (
-        <p>Loading projects...</p>
-      )}
-      {selectedProject && (
-        <ProjectModal
-          project={selectedProject}
-          onClose={() => setSelectedProject(null)}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div id="projects" className="max-w-[1040px] m-auto px-4 py-0 md:pl-20">
+            <h1 className="text-4xl font-bold text-center text-blue-900 mb-8">
+                Projects
+            </h1>
+            {projects.length > 0 ? (
+                <Carousel
+                    withIndicators
+                    withControls
+                    controlsOffset="xl"
+                    controlSize={40}
+                    nextControlIcon={
+                        <IconArrowRight
+                            style={{ width: rem(26), height: rem(26) }}
+                        />
+                    }
+                    previousControlIcon={
+                        <IconArrowLeft
+                            style={{ width: rem(26), height: rem(26) }}
+                        />
+                    }
+                    slideSize="80%"
+                    slideGap="lg"
+                    styles={{
+                        control: {
+                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                            borderRadius: '9999px',
+                            width: '50px',
+                            height: '50px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            color: 'white',
+                        },
+                        indicator: {
+                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        },
+                    }}
+                >
+                    {projects.map((project) => (
+                        <Carousel.Slide key={project.title}>
+                            <ProjectItem
+                                image={project.mainImage}
+                                title={project.title}
+                                description={project.companyName}
+                                onMoreInfo={() => setSelectedProject(project)}
+                            />
+                        </Carousel.Slide>
+                    ))}
+                </Carousel>
+            ) : (
+                <p>Loading projects...</p>
+            )}
+            {selectedProject && (
+                <ProjectModal
+                    project={selectedProject}
+                    onClose={() => setSelectedProject(null)}
+                />
+            )}
+        </div>
+    );
 };
 
 export default Projects;
